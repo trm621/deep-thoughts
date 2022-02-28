017582e658c382bc05b8df2f35025710debbbd72
trm621 <t.maher621@gmail.com>
2022-02-28 01:47:43 -0500

    user authentication

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
-
+  const [login, { error }] = useMutation(LOGIN_USER);
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -13,16 +14,20 @@ const Login = (props) => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+// submit form
+const handleFormSubmit = async event => {
+  event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
+  try {
+    const { data } = await login({
+      variables: { ...formState }
     });
-  };
+  
+    Auth.login(data.login.token);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -53,6 +58,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
