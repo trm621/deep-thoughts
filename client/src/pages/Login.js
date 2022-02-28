48b7baf5fc8316b8367ff243a03db8f8c60111da
trm621 <t.maher621@gmail.com>
2022-02-28 02:04:43 -0500

    client form submission

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 
 const Login = (props) => {
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,11 +4,14 @@ import Auth from '../utils/auth';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -33,13 +36,29 @@ const Profile = () => {
       </h4>
     );
   }
-  
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
-      <h2 className="bg-dark text-secondary p-3 display-inline-block">
-       Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-      </h2>
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+
+        {userParam && (
+  <button className="btn ml-auto" onClick={handleClick}>
+    Add Friend
+  </button>
+)}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -55,6 +74,7 @@ const Profile = () => {
     />
   </div>
 </div>
+<div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
